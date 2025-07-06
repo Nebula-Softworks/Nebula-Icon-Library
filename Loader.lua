5c61e8a7b8e1fbad5b9eb23357431a640d6c00e4
hunt3r <lyzethan@gmail.com>
2025-07-06 13:55:22 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,48 +1,10 @@
 local module = {}
 
-local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/master/MaterialIcons.lua"))()
-local Lucide = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/master/LucideIcons.lua"))()
+module.Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/master/MaterialIcons.lua"))()
+module.Lucide = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/master/LucideIcons.lua"))()
 
 function module:GetIcon(name, source)
-	source = source or "Material"
-
-	if source == "Lucide" then
-		-- full credit to latte softworks :)
-		local icons = Lucide
-			icon = string.match(string.lower(icon), "^%s*(.*)%s*$") :: string
-			local sizedicons = icons['48px']
-
-			local r = sizedicons[icon]
-			if not r then
-				error("Failed to find icon by the name of \"" .. icon .. "\.", 2)
-			end
-
-			local rirs = r[2]
-			local riro = r[3]
-
-			if type(r[1]) ~= "number" or type(rirs) ~= "table" or type(riro) ~= "table" then
-				error("Lucide Icons: Internal error: Invalid auto-generated asset entry")
-			end
-
-			local irs = Vector2.new(rirs[1], rirs[2])
-			local iro = Vector2.new(riro[1], riro[2])
-
-			local asset = {
-				id = r[1],
-				imageRectSize = irs,
-				imageRectOffset = iro,
-			}
-
-			return asset
-			--return icons[name]
-	elseif source == "Material" then	
-		if name ~= nil then
-			return Material[name]
-		else
-      error("Failed to find icon by the name of \"" .. icon .. "\.", 2)
-			return nil
-		end
-	end
+	return module[source][name]
 end
 
 return module
