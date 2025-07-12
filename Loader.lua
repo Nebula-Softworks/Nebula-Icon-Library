96b18ca0335e1537baa421f7c2b2dfde1133fef0
hunt3r <lyzethan@gmail.com>
2025-07-12 16:16:02 +0800

    phosphor icons

--- Loader.lua
+++ Loader.lua
@@ -2,6 +2,7 @@ local module = {}
 
 module.Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/master/MaterialIcons.lua"))()
 module.Lucide = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/master/LucideIcons.lua"))()
+module.Phosphor = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/refs/heads/master/Phosphor.lua"))()
 
 function module:GetIcon(name, source)
 	source = source or "Material"
