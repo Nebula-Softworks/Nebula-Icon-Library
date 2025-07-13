f8478985173afd09cb3f1a84bfde07df60cced88
hunt3r <lyzethan@gmail.com>
2025-07-13 12:05:58 +0800

    Update Loader.lua For Phosphor Filled

--- Loader.lua
+++ Loader.lua
@@ -3,6 +3,7 @@ local module = {}
 module.Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/master/MaterialIcons.lua"))()
 module.Lucide = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/master/LucideIcons.lua"))()
 module.Phosphor = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/refs/heads/master/Phosphor.lua"))()
+moduke["Phosphor-Filled"] = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/refs/heads/master/Phosphor%20Filled.lua"))()
 
 function module:GetIcon(name, source)
 	source = source or "Material"
