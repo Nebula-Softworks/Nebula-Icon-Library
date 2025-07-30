c0e73672924113da8d3105f95d1281a355f5f0b6
hunt3r <lyzethan@gmail.com>
2025-07-30 19:09:37 +0800

    SF Symbols

--- Loader.lua
+++ Loader.lua
@@ -4,6 +4,7 @@ module.Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Neb
 module.Lucide = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/master/LucideIcons.lua"))()
 module.Phosphor = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/refs/heads/master/Phosphor.lua"))()
 module["Phosphor-Filled"] = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/refs/heads/master/Phosphor%20Filled.lua"))()
+module.SF = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Nebula-Icon-Library/refs/heads/master/SFSymbols.lua"))()
 
 function module:GetIcon(name, source)
 	source = source or "Material"
